dd0fb344b098fd6c7c37bb33001cd8ae6a1e4bf4
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2022-02-13 10:38:49 +0100

    Use SOCKSProxyManager for the http client if a proxy_server is specified.

--- datajoint/s3.py
+++ datajoint/s3.py
@@ -2,51 +2,86 @@
 AWS S3 operations
 """
 from io import BytesIO
-import minio   # https://docs.minio.io/docs/python-client-api-reference
+import minio  # https://docs.minio.io/docs/python-client-api-reference
+import urllib3
 import warnings
 import uuid
 from pathlib import Path
 from . import errors
+from urllib3.contrib.socks import SOCKSProxyManager
 
 
 class Folder:
     """
     A Folder instance manipulates a flat folder of objects within an S3-compatible object store
     """
-    def __init__(self, endpoint, bucket, access_key, secret_key, *, secure=False, **_):
-        self.client = minio.Minio(endpoint, access_key=access_key, secret_key=secret_key,
-                                  secure=secure)
+
+    def __init__(
+        self,
+        endpoint,
+        bucket,
+        access_key,
+        secret_key,
+        *,
+        secure=False,
+        proxy_server=None,
+        **_
+    ):
+        self.client = minio.Minio(
+            endpoint,
+            access_key=access_key,
+            secret_key=secret_key,
+            secure=secure,
+            http_client=(
+                SOCKSProxyManager(
+                    proxy_server,
+                    timeout=urllib3.Timeout.DEFAULT_TIMEOUT,
+                    cert_reqs="CERT_REQUIRED",
+                    retries=urllib3.Retry(
+                        total=5,
+                        backoff_factor=0.2,
+                        status_forcelist=[500, 502, 503, 504],
+                    ),
+                )
+                if proxy_server
+                else None
+            ),
+        )
         self.bucket = bucket
         if not self.client.bucket_exists(bucket):
-            raise errors.BucketInaccessible('Inaccessible s3 bucket %s' % bucket) from None
+            raise errors.BucketInaccessible(
+                "Inaccessible s3 bucket %s" % bucket
+            ) from None
 
     def put(self, name, buffer):
         return self.client.put_object(
-            self.bucket, str(name), BytesIO(buffer), length=len(buffer))
+            self.bucket, str(name), BytesIO(buffer), length=len(buffer)
+        )
 
     def fput(self, local_file, name, metadata=None):
         return self.client.fput_object(
-            self.bucket, str(name), str(local_file), metadata=metadata)
+            self.bucket, str(name), str(local_file), metadata=metadata
+        )
 
     def get(self, name):
         try:
             return self.client.get_object(self.bucket, str(name)).data
         except minio.error.NoSuchKey:
-            raise errors.MissingExternalFile('Missing s3 key %s' % name) from None
+            raise errors.MissingExternalFile("Missing s3 key %s" % name) from None
 
     def fget(self, name, local_filepath):
         """get file from object name to local filepath"""
         name = str(name)
         stat = self.client.stat_object(self.bucket, name)
-        meta = {k.lower().lstrip('x-amz-meta'): v for k, v in stat.metadata.items()}
+        meta = {k.lower().lstrip("x-amz-meta"): v for k, v in stat.metadata.items()}
         data = self.client.get_object(self.bucket, name)
         local_filepath = Path(local_filepath)
         local_filepath.parent.mkdir(parents=True, exist_ok=True)
-        with local_filepath.open('wb') as f:
+        with local_filepath.open("wb") as f:
             for d in data.stream(1 << 16):
                 f.write(d)
-        if 'contents_hash' in meta:
-            return uuid.UUID(meta['contents_hash'])
+        if "contents_hash" in meta:
+            return uuid.UUID(meta["contents_hash"])
 
     def exists(self, name):
         try:
@@ -65,4 +100,4 @@ class Folder:
         try:
             self.client.remove_object(self.bucket, str(name))
         except minio.ResponseError:
-            return errors.DataJointError('Failed to delete %s from s3 storage' % name)
+            return errors.DataJointError("Failed to delete %s from s3 storage" % name)
